ef10ebce8081b203518cf804f724cf112b77995e
1902051 <36678603+1902051@users.noreply.github.com>
2018-12-02 13:07:43 -0800

    Happy

---
+++ src/loops/Hapopoy.java
@@ -0,0 +1,31 @@
+package loops;
+
+import javax.swing.JOptionPane;
+
+public class Hapopoy {
+
+public static void main(String[] args) {
+	
+int jaker = JOptionPane.showConfirmDialog(null, "Are you happy");
+	
+if (jaker == 1){
+	JOptionPane.showConfirmDialog(null, "Do you want to be happy");
+	
+}
+	
+
+if (jaker == 1){
+	JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+}
+
+if (jaker == 0 {
+	
+}
+
+
+}
+}
+
+
+//yes = 0
+//no = 1
